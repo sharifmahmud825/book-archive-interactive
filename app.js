00043e107d7529854ac8b32ce2ba04c79f2b5bbd
unknown <sharifmahmud825@gmail.com>
2021-09-02 02:36:08 +0600

    getting data from api added

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
 // Assign Value
 const inputField = document.getElementById('input-field');
-const searchField = inputField.value;
-
+const bookContainer = document.getElementById('book-container');
+const errorDiv = document.getElementById('error');
+const errorContainer = document.getElementById('error-container');
+const resultContainer = document.getElementById('result-container');
 
 const loadData = () => {
-    fetch(`http://openlibrary.org/search.json?q=${'searchField'}`)
+    const search = inputField.value;
+// condition for empty search
+    if(search === ''){
+        const h2 = document.createElement('h2');
+        h2.classList.add('text-center');
+        h2.innerHTML=`
+        <h2 class="text-center my-5">
+            No result Found
+        </h2>
+        `;
+        errorContainer.appendChild(h2);
+        return;
+    }
+    
+
+// clear display data
+    bookContainer.textContent='';
+    errorContainer.textContent= '';
+    
+// Get API
+ 
+    fetch(`http://openlibrary.org/search.json?q=${inputField.value}`)
     .then(res => res.json())
-    .then(data => console.log(data.docs[0]))
+    .then(data => displayData(data.docs))
+}
+
+const displayData = books => {
+    console.log(books);
+
+    books.forEach(book => {
+        // Clear
+       inputField.value= '';
+// Create new div
+
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="col">
+            <div class="card h-100">
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"  class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">Author : ${book.author_name}</p>
+                    <p class="card-text">First Publish year : ${book.first_publish_year}</p>
+                    <p class="card-text">Publisher : ${book.publisher}</p>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-success w-100">Borrow</button>
+                </div>
+            </div>
+        </div>
+        `;
+        bookContainer.appendChild(div);  
+    });
+    
 }
